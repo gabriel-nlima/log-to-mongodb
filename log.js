f03d097608f140c95d99e6885c02df3bed0f66bf
Gabriel Nascimento Lima <gabriel.lima13372@gmail.com>
2019-04-06 17:57:32 -0300

    Create log.js

---
+++ log.js
@@ -0,0 +1,19 @@
+"use strict";
+
+module.exports = function log(data) {
+  let log;
+
+  try {
+    log = JSON.parse(data);
+
+    if (log.time) {
+      log.time = new Date(log.time);
+    }
+  } catch (e) {
+    log = {
+      msg: data
+    };
+  }
+
+  return log;
+};
