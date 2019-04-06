c9522700ce2fa3b0e1fecbd92d3eb6046eb9968d
Gabriel Nascimento Lima <gabriel.lima13372@gmail.com>
2019-04-06 17:58:19 -0300

    Create makeInsert.js

---
+++ makeInsert.js
@@ -0,0 +1,36 @@
+"use strict";
+
+const EOL = require("os").EOL;
+const options = {
+  forceServerObjectId: true
+};
+
+module.exports = function makeInsert(showErrors, showStdout) {
+  let callback;
+
+  if (showErrors && showStdout) {
+    callback = function(e, result) {
+      if (e) {
+        console.error(e);
+      } else {
+        process.stdout.write(JSON.stringify(result.ops[0]) + EOL);
+      }
+    };
+  } else if (showErrors && !showStdout) {
+    callback = function(e) {
+      if (e) {
+        console.error(e);
+      }
+    };
+  } else if (!showErrors && showStdout) {
+    callback = function(e, result) {
+      if (!e) {
+        process.stdout.write(JSON.stringify(result.ops[0]) + EOL);
+      }
+    };
+  }
+
+  return function insert(collection, log) {
+    collection.insertOne(log, options, callback);
+  };
+};
